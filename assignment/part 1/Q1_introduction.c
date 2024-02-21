abc50ebebd6884f7eb7134dae322e8eb1227be48
Rshroff2036 <156286973+Rshroff2036@users.noreply.github.com>
2024-02-21 23:07:22 +0530

    Delete assignment/part 1 directory

--- assignment/part 1/Q1_introduction.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	printf("\n My name is RAHUL SHROFF");
-	printf("\n Birth date : 26th sep 1998");
-	printf("\n I m 25 years old");
-	printf("\n Address : D\1, kalindi Appartment D-cabin Sabarmati Ahmedabad - 380019");
-	return 0;
-}
