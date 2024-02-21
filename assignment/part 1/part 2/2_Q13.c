3649b34e5cf84be045d84a00eca152a0011834a0
Rshroff2036 <156286973+Rshroff2036@users.noreply.github.com>
2024-02-21 23:05:42 +0530

    Delete assignment/part 1/part 2 directory

--- assignment/part 1/part 2/2_Q13.c
+++
@@ -1,9 +0,0 @@
-//WAP to find minimum number among 3 numbers using ternary operator
-#include<stdio.h>
-int main()
-{
-	int n1=25,n2=87,n3=14,minimum;
-	minimum=(n1<n2)?(n1<n3?n1:n3):(n2<n3?n2:n3);
-	printf("minimum number is %d %d and %d is %d",n1,n2,n3,minimum);
-	return 0;
-}
